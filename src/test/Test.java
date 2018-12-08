60dc0393c2cc2a2711cb0384050aa468785696ea
Andrii <andrii.yeremenko@gmail.com>
2018-12-08 22:39:55 +0200

    Added CpuSpecificaton class with new features

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,12 @@
 package test;
 
+import main.general.CpuPerfomance.CpuSpecification;
 import main.general.OSPerfomance.SystemInfo;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(CpuSpecification.getLogicalNumberCPUCores());
+        System.out.println(CpuSpecification.getPhysycalNumberCPUCores());
+
+    }
 }
